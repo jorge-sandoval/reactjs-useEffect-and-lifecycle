ab2ea48f3d5f1257405071cbd8834229725b38e6
Jorge O. Sandoval <jsandoval@truelogic.io>
2024-02-11 17:35:48 -0500

    efects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [age, setAge] = useState(0);
   const [name, setName] = useState('');
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    document.title = `${name}`;
+  }, [name]);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    // Clean up
+    return () => {
+      window.removeEventListener('resize', () => handleResize);
+    };
+  }, []);
 
   return (
     <>
@@ -19,6 +34,9 @@ function App() {
         <button onClick={() => setAge(age + 1)}>+</button>
         <br />
         My name is {name} and I am {age} years old.
+      </div>
+      <div>
+        <h1>Window Width: {width}</h1>
       </div>
     </>
   );
